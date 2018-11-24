1be16cb239d9690fdd046425401182eb003e1e57
Julien Midedji <Julien.Midedji@gmail.com>
2018-11-24 13:51:32 +0100

    Rework entry component registration to conform to open closed principle

--- src/components/Homepage.vue
+++ src/components/Homepage.vue
@@ -3,68 +3,31 @@
     <Header />
     <main>
       <div v-infinite-scroll="loadMore" infinite-scroll-disabled="busy" infinite-scroll-distance="10">
-        <Schlossparklauf18 v-if="visible[13]" />
-        <Ytpmv v-if="visible[12]" />
-        <Pastmemories v-if="visible[11]" />
-        <Spayle v-if="visible[10]" />
-        <Jubilaeum v-if="visible[9]" />
-        <Rheinuferlauf17 v-if="visible[8]" />
-        <Apfelkuchen v-if="visible[7]" />
-        <Charityrun17 v-if="visible[6]" />
-        <Kaesekuchen v-if="visible[5]" />
-        <Screenbounce v-if="visible[4]" />
-        <Bouncingball v-if="visible[3]" />
-        <Chocolatechipcookies v-if="visible[2]" />
-        <Start v-if="visible[1]" />
-        <Erster v-if="visible[0]" />
+        <component
+          v-for="i in entries.length"
+          :key="i"
+          :is="entries[i]"
+          v-if="visible[i]" />
       </div>
     </main>
   </div>
 </template>
 
 <script>
-import Schlossparklauf18 from '@/entries/Schlossparklauf18.vue';
-import Pastmemories from '@/entries/Pastmemories.vue';
-import Spayle from '@/entries/Spayle.vue';
-import Jubilaeum from '@/entries/Jubilaeum.vue';
-import Rheinuferlauf17 from '@/entries/Rheinuferlauf17.vue';
-import Apfelkuchen from '@/entries/Apfelkuchen.vue';
-import Charityrun17 from '@/entries/Charityrun17.vue';
-import Kaesekuchen from '@/entries/Kaesekuchen.vue';
-import Screenbounce from '@/entries/Screenbounce.vue';
-import Bouncingball from '@/entries/Bouncingball.vue';
-import Chocolatechipcookies from '@/entries/Chocolatechipcookies.vue';
-import Start from '@/entries/Start.vue';
-import Erster from '@/entries/Erster.vue';
-import Ytpmv from '@/entries/Ytpmv.vue';
+import { order as entries } from '@/entries';
 import Header from './Header.vue';
-
-const ENTRY_COUNT = 13;
 
 export default {
   name: 'homepage',
-  components: {
+  components: { 
     Header,
-    Schlossparklauf18,
-    Pastmemories,
-    Spayle,
-    Jubilaeum,
-    Rheinuferlauf17,
-    Apfelkuchen,
-    Charityrun17,
-    Kaesekuchen,
-    Screenbounce,
-    Bouncingball,
-    Chocolatechipcookies,
-    Start,
-    Erster,
-    Ytpmv,
   },
   data() {
     return {
-      index: ENTRY_COUNT,
-      visible: new Array(ENTRY_COUNT).fill(false),
+      index: -1, // -1 = No entries are shown
+      visible: new Array(entries.length).fill(false),
       busy: false,
+      entries,
     };
   },
   methods: {
@@ -72,11 +35,9 @@ export default {
       this.busy = true;
 
       setTimeout(() => {
-        if (this.index >= 0) {
-          this.$set(this.visible, this.index, true);
-          this.index -= 1;
-          this.busy = false;
-        }
+        this.$set(this.visible, this.index, true); // arrays cannot be set directly responsively in vue
+        this.index += 1;
+        this.busy = false;
       }, 10);
     },
   },
--- src/main.js
+++ src/main.js
@@ -4,25 +4,11 @@ import infiniteScroll from 'vue-infinite-scroll';
 
 import App from './App.vue';
 import Homepage from './components/Homepage.vue';
-import SoloEntry from './components/SoloEntry.vue';
 import AboutUs from './components/AboutUs.vue';
 
-import Schlossparklauf18 from './entries/Schlossparklauf18.vue';
-import Pastmemories from './entries/Pastmemories.vue';
-import Spayle from './entries/Spayle.vue';
-import Jubilaeum from './entries/Jubilaeum.vue';
-import Rheinuferlauf17 from './entries/Rheinuferlauf17.vue';
-import Apfelkuchen from './entries/Apfelkuchen.vue';
-import Charityrun17 from './entries/Charityrun17.vue';
-import Kaesekuchen from './entries/Kaesekuchen.vue';
-import Screenbounce from './entries/Screenbounce.vue';
-import Bouncingball from './entries/Bouncingball.vue';
-import Chocolatechipcookies from './entries/Chocolatechipcookies.vue';
-import Start from './entries/Start.vue';
-import Erster from './entries/Erster.vue';
-import Ytpmv from './entries/Ytpmv.vue';
-
 import './global.css';
+
+import { routes as entries } from './entries';
 
 Vue.config.productionTip = false;
 
@@ -34,26 +20,7 @@ const router = new VueRouter({
   routes: [
     { path: '/', component: Homepage },
     { path: '/impressum', component: AboutUs },
-    {
-      path: '/eintrag',
-      component: SoloEntry,
-      children: [
-        { path: 'schlossparklauf18', component: Schlossparklauf18 },
-        { path: 'past-memories', component: Pastmemories },
-        { path: 'spayle', component: Spayle },
-        { path: 'jubilaeum', component: Jubilaeum },
-        { path: 'rheinuferlauf', component: Rheinuferlauf17 },
-        { path: 'apfelkuchen', component: Apfelkuchen },
-        { path: 'charityrun', component: Charityrun17 },
-        { path: 'kaesekuchen', component: Kaesekuchen },
-        { path: 'screenbounce', component: Screenbounce },
-        { path: 'bouncingball', component: Bouncingball },
-        { path: 'chocolatechipcookies', component: Chocolatechipcookies },
-        { path: 'start', component: Start },
-        { path: 'erster', component: Erster },
-        { path: 'ytpmv', component: Ytpmv },
-      ],
-    },
+    ...entries,
   ],
 });
 
